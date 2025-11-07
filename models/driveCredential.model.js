4f34a35c78433c1fa9d0bd186cb2f56777b0203f
krishnaprogrammer <mac@macs-MacBook-Pro.local>
2025-11-07 17:52:22 +0530

    Converted driveCredential model to CommonJS syntax for Rendr

--- models/driveCredential.model.js
+++ models/driveCredential.model.js
@@ -1,11 +1,15 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const DriveCredentialSchema = new mongoose.Schema({
-  clientId: { type: String, required: true },
-  clientSecret: { type: String, required: true },
-  apiKey: { type: String, required: true },
-  scopes: { type: String, required: true },
-}, { timestamps: true });
+const DriveCredentialSchema = new mongoose.Schema(
+  {
+    clientId: { type: String, required: true },
+    clientSecret: { type: String, required: true },
+    apiKey: { type: String, required: true },
+    scopes: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("DriveCredential", DriveCredentialSchema);
+const DriveCredential = mongoose.model("DriveCredential", DriveCredentialSchema);
 
+module.exports = DriveCredential;
